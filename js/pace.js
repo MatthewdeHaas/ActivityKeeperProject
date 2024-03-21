547ac8ef633325b5227840fccceb990bb5c6ae6c
Matthew deHaas <mattdehaas28@gmail.com>
2024-03-21 10:20:26 -0700

    Fully implemented te pace conversion calculator

--- js/pace.js
+++ js/pace.js
@@ -1,3 +1,6 @@
+const btn_col = "#f4511e"
+const pressed_btn_col = "#b26148";
+
 // input: an array of length three containing the hours, minutes, and seconds, in that order
 // output: base ten equivalent of the given time in minutes
 function time_to_decimal(time) {
@@ -17,10 +20,51 @@ function get_speed(distance, time) {
     return distance / (time_to_decimal(time) / 60);
 }
 
+
+// input: distance in km, time array of length three with [hours, mins, secs], and optional units ("km" or "mile")
+// output: a time array of [hours, mins, secs] in mins/km or mins/mile
+function get_pace(distance, time, units="km") {
+
+    // bounds and validation checking
+    for (let i = 0; i < time.length; i++) if (isNaN(time[i])) return [NaN];
+    if (time[1] < 0 || time[1] > 60 || time[2] < 0 || time[2] > 60) return [NaN];
+
+    let conversion_factor = 1;
+    if (units.trim().toLowerCase() == "mile") conversion_factor = 1.609; // mile conversion factor
+
+    // pace = time / distance
+    return [decimal_to_time(time_to_decimal(time) / distance * conversion_factor)];
+
+}
+
+function get_distance(pace, time, units="km") {
+
+    for (let i = 0; i < time.length; i++) if (isNaN(time[i])) return [NaN];
+    if (time[1] < 0 || time[1] > 60 || time[2] < 0 || time[2] > 60) return [NaN];
+
+    let conversion_factor = 1;
+    if (units.trim().toLowerCase() == "mile") conversion_factor = 1.609; // mile conversion factor
+
+    // distance = time / pace
+    return time_to_decimal(time) / time_to_decimal(pace) * conversion_factor;
+
+}
+
+function get_time(pace, distance, units="km") {
+
+    let conversion_factor = 1;
+    if (units.trim().toLowerCase() == "mile") conversion_factor = 1.609; // mile conversion factor
+
+    // time = pace * distance
+    return [decimal_to_time(time_to_decimal(pace) * distance)];
+
+}
+
+
 // input: time is a array of 3 values: [hours, mins, secs] where each are within their respective domains, units ("km" or "mile"),
 // and a range (boolean)
 // output: a time array or an array of two values if range is true
-function get_pace(time, units="km", speed="easy", range=false) {
+function calculate_pace(time, units="km", speed="easy", range=false) {
     // bounds and validation checking
     for (let i = 0; i < time.length; i++) if (isNaN(time[i])) return [NaN];
     if (time[1] < 0 || time[1] > 60 || time[2] < 0 || time[2] > 60) return [NaN];
@@ -40,7 +84,9 @@ function get_pace(time, units="km", speed="easy", range=false) {
     }
   }
 
-// input: an array of length three containing the hours, minutes, and seconds, in that order
+// input: 2D array of legnth 1 for a single time or length for a range of times
+// each array contains the hours, minutes, and seconds, in that order
+// eg. [[0, 34, 14]] for a single time and [[1, 30, 11], [2, 56, 10]] for a range from the first time to the second
 function format_time(time) {
 
     // adds a zero to any single digit number
@@ -78,10 +124,10 @@ function submit_pb() {
     let easy_pace_text = document.getElementById("easy-pace-label").innerText;
     //let easy_pace_range_text = document.getElementById("easy-pace-range-label").innerText;
 
-    document.getElementById("10k-pace-label").innerText = tenkm_pace_text.substring(0, tenkm_pace_text.indexOf(":") + 1) + " " + format_time(get_pace(time, units, speed="10k", range=true)) + " min/" + units.toString();
-    document.getElementById("tempo-pace-label").innerText = tempo_pace_text.substring(0, tempo_pace_text.indexOf(":") + 1) + " " + format_time(get_pace(time, units, speed="tempo", range=true)) +  " min/" + units.toString();
-    document.getElementById("easy-pace-label").innerText = easy_pace_text.substring(0, easy_pace_text.indexOf(":") + 1) + " " + format_time(get_pace(time, units, speed="easy", range=false)) + " min/" + units.toString();
-    //document.getElementById("easy-pace-range-label").innerText = easy_pace_range_text.substring(0, easy_pace_range_text.indexOf(":") + 1) + " " + format_time(get_pace(time, units, speed="easy", range=true)) + " min/" + units;
+    document.getElementById("10k-pace-label").innerText = tenkm_pace_text.substring(0, tenkm_pace_text.indexOf(":") + 1) + " " + format_time(calculate_pace(time, units, speed="10k", range=true)) + " min/" + units.toString();
+    document.getElementById("tempo-pace-label").innerText = tempo_pace_text.substring(0, tempo_pace_text.indexOf(":") + 1) + " " + format_time(calculate_pace(time, units, speed="tempo", range=true)) +  " min/" + units.toString();
+    document.getElementById("easy-pace-label").innerText = easy_pace_text.substring(0, easy_pace_text.indexOf(":") + 1) + " " + format_time(calculate_pace(time, units, speed="easy", range=false)) + " min/" + units.toString();
+    //document.getElementById("easy-pace-range-label").innerText = easy_pace_range_text.substring(0, easy_pace_range_text.indexOf(":") + 1) + " " + format_time(calculate_pace(time, units, speed="easy", range=true)) + " min/" + units;
   }
 
 
@@ -142,5 +188,52 @@ function adjust_pace_buttons(conv_type) {
             time_div.setAttribute("hidden", "hidden");
             break;
     }
+}
+
+
+function submit_conversion(){
+
+    // labels
+    let pace_lbl = document.getElementById("pace_lbl");
+    let dist_lbl = document.getElementById("dist_lbl");
+    let time_lbl = document.getElementById("time_lbl");
+
+    let pace = [0, document.getElementById("pace_min_inp").value, document.getElementById("pace_sec_inp").value];
+    for (let i = 0; i < pace.length; i++) if (pace[i] == "") pace[i] = 0;
+    pace = pace.map((x) => parseInt(x));
+
+    let dist = document.getElementById("dist_inp").value;
+
+    let time = [document.getElementById("time_hr_inp").value, document.getElementById("time_min_inp").value, document.getElementById("time_sec_inp").value];
+    for (let i = 0; i < time.length; i++) if (time[i] == "") time[i] = 0;
+    time = time.map((x) => parseInt(x));
+
+    let units = "km"
+
+    if (document.getElementById("pace_btn").style.backgroundColor.toString() == "rgb(178, 97, 72)") {
+        pace_lbl.removeAttribute("hidden");
+        dist_lbl.setAttribute("hidden", "hidden");
+        time_lbl.setAttribute("hidden", "hidden");
+
+        console.log(dist);
+        console.log(time);
+
+        document.getElementById("pace_lbl").innerText = pace_lbl.innerText.substring(0, pace_lbl.innerText.indexOf(":") + 1) + " " + format_time(get_pace(dist, time)) + " min/" + units;
+    }
+    if (document.getElementById("dist_btn").style.backgroundColor.toString() == "rgb(178, 97, 72)") {
+        pace_lbl.setAttribute("hidden", "hidden");
+        dist_lbl.removeAttribute("hidden");
+        time_lbl.setAttribute("hidden", "hidden");
+
+        document.getElementById("dist_lbl").innerText = dist_lbl.innerText.substring(0, dist_lbl.innerText.indexOf(":") + 1) + " " + get_distance(pace, time) + " " + units;
+    }
+    if (document.getElementById("time_btn").style.backgroundColor.toString() == "rgb(178, 97, 72)") {
+        pace_lbl.setAttribute("hidden", "hidden");
+        dist_lbl.setAttribute("hidden", "hidden");
+        time_lbl.removeAttribute("hidden");
+
+       document.getElementById("time_lbl").innerText = time_lbl.innerText.substring(0, time_lbl.innerText.indexOf(":") + 1) + " " + format_time(get_time(pace, dist));
+    }
+
 }
 
